6a26ab622b7270c45d2af57800285de29ab21e98
gbhmh <46986168+gbhmh@users.noreply.github.com>
2019-06-04 12:28:08 -0500

    icp1
    icp1

---
+++ delchar.py
@@ -0,0 +1,8 @@
+# input a string
+string1 = input('enter a word : ')
+# deleting two characters
+string2 = string1[:3] + string1[5:]
+# reversing the string
+string3 = string2[::-1]
+# printing output in console
+print('reverse result is : ' + string3)
---
+++ replace.py
@@ -0,0 +1,6 @@
+# input a sentence
+string1 = input('enter a sentence : ')
+# replacing the string
+string2 = string1.replace("python", "pythons")
+# printing the result
+print('output sentence is : ' + string2)
